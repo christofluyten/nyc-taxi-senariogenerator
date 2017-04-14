00ac6ea5c5ec02686e081e67660604974f4c1f1f
ChristofLuyten <christof.luyten@student.kuleuven.be>
2017-04-14 14:00:02 +0200

    main.sh

---
+++ main-experiment.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+export MAVEN_OPTS=-server
+nohup mvn clean compile -U exec:java -Dexec.mainClass="com.github.christof.experiment.NycExperiment" -Dexec.args="-exp \
+vanlon15 \
+MAIN_CONFIGS \
+--show-gui false \
+--repetitions 3 \
+--scenarios.filter glob:**.scen \
+--warmup 30000 \
+--ordering SEED_REPS,REPS,SCENARIO,CONFIG" &
