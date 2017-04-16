438a8fc5bb813d63c0203b2132eccba73196671e
Christof Luyten <christof.luyten@student.kuleuven.be>
2017-04-16 12:53:24 +0200

    configs

--- main-experiment.sh
+++ main-experiment.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-export MAVEN_OPTS=-Xmx4g -server
+export MAVEN_OPTS="-Xmx4g -server"
 nohup mvn clean compile -U exec:java -Dexec.mainClass="com.github.christof.experiment.NycExperiment" -Dexec.args="false" &
